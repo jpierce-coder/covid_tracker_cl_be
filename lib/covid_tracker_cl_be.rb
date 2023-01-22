85cac40ee7e46299904a76a28503312682981a10
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-01-21 20:13:30 -0600

    Base Files for COVID Tracker Ruby Gem
    - Created base file structure for Ruby Gem

---
+++ lib/covid_tracker_cl_be.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "covid_tracker_cl_be/version"
+
+module CovidTrackerClBe
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "covid_tracker_cl_be"
+
+require "minitest/autorun"
