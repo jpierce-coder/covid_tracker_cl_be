03613dd7c7d0f99be407a16381bba4b8b0b67d2f
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-01-22 17:18:59 -0600

    Finished Covid Tracker Gem
    - Added functionality to display choices in CLI scraper of:
    -- USA information
    -- Listing all states information
    -- Top 10 states with most cases
    -- Top 10 states with least cases

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,14 @@ require 'nokogiri'
 require 'open-uri'
 require_relative "printable.rb"
 require_relative "country.rb"
-
+require_relative "state.rb"
 class Scraper
   extend Printable::Format  
   SCRAPE_URL = "https://www.worldometers.info/coronavirus/country/us/"
   
   def self.scrape_usa
-    doc = Nokogiri::HTML(URI.open("https://www.worldometers.info/coronavirus/country/us/"))
+    puts "Scraping US information.."
+    doc = Nokogiri::HTML(URI.open(SCRAPE_URL))
     usa_confirmed_cases = text_to_integer(doc.css(".maincounter-number")[0].text)
     usa_overall_deaths = text_to_integer(doc.css(".maincounter-number")[1].text)
     usa_recoveries = text_to_integer(doc.css(".maincounter-number")[2].text)
@@ -22,8 +23,28 @@ class Scraper
   end
 
   def self.scrape_states
+    puts "Scraping US States information..."
+    doc = Nokogiri::HTML(URI.open(SCRAPE_URL))
+
+    doc.css('tbody tr')[1..51].each do |state_row|
+      row = state_row.css('td')
+      name = row[1].text.split(" ").join(" ")
+      cases = text_to_integer(row[2].text)
+      deaths = text_to_integer(row[4].text)
+      recoveries = text_to_integer(row[6].text)
+
+      if name != "District Of Columbia"
+        State.new(name, cases, deaths, recoveries)
+      end
+
+
+    end
 
   end
-end
 
+  def self.scrape_information
+    scrape_usa
+    scrape_states
+  end
+end
 
